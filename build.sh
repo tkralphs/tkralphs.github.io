8422b441608ffede4148415751c8675eb4e7a5c2
Ted Ralphs <tkralphs@gmail.com>
2016-01-11 13:15:03 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ rm -rf ../tkralphs.github.io.master
 git clone https://${GH_TOKEN}@github.com/tkralphs/tkralphs.github.io.git ../tkralphs.github.io.master
 
 # copy generated HTML site to `master' branch
-cp -R _site/* ../tkralphs.github.io.master/_site
+cp -R _site/* ../tkralphs.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
